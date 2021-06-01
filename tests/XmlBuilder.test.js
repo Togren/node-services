373c10cb9205b1402eacdd85313b8543607c9ade
Tomas Schlepers <t.schlepers@outlook.com>
2021-06-01 09:17:17 +0200

    Start on unit tests

---
+++ tests/XmlBuilder.test.js
@@ -0,0 +1,24 @@
+
+const XmlBuilder = require('XmlBuilder');
+
+describe('XmlBuilder.fromJSON', () => {
+    describe('given not a string or object', () => {
+        test('')
+    })
+    describe('given a string', () => {
+        test('that is invalid JSON throws error', () => {
+            expect()
+        })
+        test('that is valid JSON returns XML string', () => {
+
+        })
+    })
+    describe('given an object', () => {
+        test('that is empty returns empty string', () => {
+
+        })
+        test('that is a valid JSON object returns XML string', () => {
+
+        })
+    })
+})
